30315874d523ad4fe88c695d5a615c1deeaf610f
Meng <mamengchn@gmail.com>
2016-05-09 14:29:09 -0500

    add comments and title

---
+++ misc/IIA.m
@@ -0,0 +1,17 @@
+function [ output_args ] = IIA( K, theta0, R, eta, epsilon )
+%IIA	Cortes Interpolated Iterative Algorithm
+%
+% Input:
+%		K		3-way tensor of kernel matrix
+%		theta0	initial theta
+%		R		a factor control theta changes
+%		eta		parameter controls alpha changes
+%		epsilon stop criterion
+% Output:
+%		alpha
+
+alpha = 
+
+
+end
+
